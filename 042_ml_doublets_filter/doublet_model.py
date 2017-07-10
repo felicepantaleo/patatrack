1c158d1670fc7b443e29056842841e3a2223634c
Antonio <w-32@hotmail.it>
2017-07-10 17:10:18 +0200

    add compatibility with API 2.0 and checkpoint loading

--- 042_ml_doublets_filter/doublet_model.py
+++ 042_ml_doublets_filter/doublet_model.py
@@ -9,7 +9,7 @@ import tempfile
 import os
 import numpy as np
 from dataset import Dataset
-from keras.layers import Input, Conv2D, MaxPooling2D, Flatten, Dense, merge, Reshape, Dropout
+from keras.layers import Input, Conv2D, MaxPooling2D, Flatten, Dense, concatenate, Reshape, Dropout
 from keras.models import Model
 from keras.utils.np_utils import to_categorical
 from keras.constraints import max_norm
@@ -19,7 +19,8 @@ from keras.callbacks import EarlyStopping, ModelCheckpoint
 DEBUG = True
 DEBUG = DEBUG and os.name == 'nt'  # DEBUG always False for server
 
-log_dir = "models/cnn_adriano/run_" + '{0:%Y-%m-%d_%H-%M-%S}'.format(datetime.datetime.now())
+t_now = '{0:%Y-%m-%d_%H-%M-%S}'.format(datetime.datetime.now())
+log_dir = "models/cnn_adriano/run_" + t_now
 remote_data = '/eos/cms/store/cmst3/group/dehep/convPixels/clean/' 
 
 train_fname = 'data/train' if not DEBUG else 'data/debug.npy'
@@ -56,13 +57,13 @@ pool = MaxPooling2D(pool_size=(2, 2), padding='same', data_format="channels_last
 
 flat = Flatten()(pool)
 #info_drop = Dropout(infos)
-concat = merge([flat, infos], mode='concat')
+concat = concatenate([flat, infos])
 
-dense = Dense(64, activation='relu', W_constraint=max_norm(2.), name='dense')(concat)
+dense = Dense(64, activation='relu', kernel_constraint=max_norm(2.), name='dense')(concat)
 drop = Dropout(DROP)(dense)
-pred = Dense(2, activation='softmax', W_constraint=max_norm(2.), name='output')(drop)
+pred = Dense(2, activation='softmax', kernel_constraint=max_norm(2.), name='output')(drop)
 
-model = Model(input=[hit_shapes, infos], output=pred)
+model = Model(inputs=[hit_shapes, infos], outputs=pred)
 model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
 model.summary()
@@ -71,18 +72,18 @@ print('Training')
 # Save the best model during validation and bail out of training early if we're not improving
 _, tmpfn = tempfile.mkstemp()
 callbacks = [EarlyStopping(patience=PATIENCE), ModelCheckpoint(tmpfn, save_best_only=True, save_weights_only=True)]
-model.fit([X_hit, X_info], y, batch_size=BATCH_SIZE, epochs=EPOCHS, validation_data=([X_val_hit, X_val_info], y_val))
+model.fit([X_hit, X_info], y, batch_size=BATCH_SIZE, epochs=EPOCHS, validation_data=([X_val_hit, X_val_info], y_val), 
+        callbacks=callbacks)
 
 # TODO: should really restore the checkpoint!
 # Restore the best found model during validation
-#model.load_weights(tmpfn)
+model.load_weights(tmpfn)
 
 loss, acc = model.evaluate([X_test_hit, X_test_info], y_test, batch_size=128)
 print('Test loss / test accuracy = {:.4f} / {:.4f}'.format(loss, acc))
 
 
-ts = '{0:%Y-%m-%d_%H-%M-%S}'.format(datetime.datetime.now())
-fname = "models/cnn_doublet/model_" + ts
+fname = "models/cnn_doublet/model_" + t_now
 model.save_weights(fname + ".h5", overwrite=True)
 with open(fname + ".json", "w") as outfile:
     json.dump(model.to_json(), outfile)
